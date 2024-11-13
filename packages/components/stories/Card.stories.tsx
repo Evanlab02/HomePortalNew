a91a007d5e9d56bb9071162f5cfe72f4b6f87c9f
Evanlab02 <evanlabuschagne70@gmail.com>
2024-11-13 10:06:11 +0200

    fix(#16): Add story in storybook for the card component

---
+++ packages/components/stories/Card.stories.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import { ContainerOutlined, DesktopOutlined, MailOutlined, PieChartOutlined } from "@ant-design/icons";
+import HpCard from "../src/Card";
+
+const meta: Meta<typeof HpCard> = {
+    component: HpCard,
+    parameters: {
+        backgrounds: {
+            default: "dark"
+        }
+    }
+};
+
+export default meta;
+type Story = StoryObj<typeof HpCard>;
+
+export const Default: Story = {
+    args: {
+        title: "Home Portal Card",
+        extra: <a>Extra Elements</a>,
+        children: <p>This is a home portal card.</p>
+    }
+};
